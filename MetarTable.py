b1114a7e70443cbe690a777c22caa650d131e1f0
eholweg <severe.wx@gmail.com>
2016-12-31 06:34:53 -0500

    Final Version For Operations

--- MetarTable.py
+++ MetarTable.py
@@ -1,82 +1,3 @@
-#
-#  This is a minimal sample script showing how the individual data
-#  are accessed from the decoded report.  To produce the standard text
-#  summary of a report, the string() method of the Metar object.
-#
-#  The parsed data are stored as attributes of a Metar object.
-#  Individual attributes are either strings. instances of one of the
-#  metar.Datatypes classes, or lists of tuples of these scalars.
-#  Here's a summary, adapted from the comments in the Metar.Metar.__init__()
-#  method:
-#
-#    Attribute       Comments [data type]
-#    --------------  --------------------
-#    code             original METAR code [string]
-#    type             METAR (routine) or SPECI (special) [string]
-#    mod              AUTO (automatic) or COR (corrected) [string]
-#    station_id       4-character ICAO station code [string]
-#    time             observation time [datetime]
-#    cycle            observation cycle (0-23) [int]
-#    wind_dir         wind direction [direction]
-#    wind_speed       wind speed [speed]
-#    wind_gust        wind gust speed [speed]
-#    wind_dir_from    beginning of range for win dir [direction]
-#    wind_dir_to      end of range for wind dir [direction]
-#    vis              visibility [distance]
-#    vis_dir          visibility direction [direction]
-#    max_vis          visibility [distance]
-#    max_vis_dir      visibility direction [direction]
-#    temp             temperature (C) [temperature]
-#    dewpt            dew point (C) [temperature]
-#    press            barometric pressure [pressure]
-#    runway           runway visibility [list of tuples...]
-#                        name [string]
-#                        low  [distance]
-#                        high [distance]
-#    weather          present weather [list of tuples...]
-#                        intensity     [string]
-#                        description   [string]
-#                        precipitation [string]
-#                        obscuration   [string]
-#                        other         [string]
-#    recent           recent weather [list of tuples...]
-#    sky              sky conditions [list of tuples...]
-#                        cover   [string]
-#                        height  [distance]
-#                        cloud   [string]
-#    windshear        runways w/ wind shear [list of strings]
-#
-#    press_sea_level  sea-level pressure [pressure]
-#    wind_speed_peak  peak wind speed in last hour [speed]
-#    wind_dir_peak    direction of peak wind speed in last hour [direction]
-#    max_temp_6hr     max temp in last 6 hours [temperature]
-#    min_temp_6hr     min temp in last 6 hours [temperature]
-#    max_temp_24hr    max temp in last 24 hours [temperature]
-#    min_temp_24hr    min temp in last 24 hours [temperature]
-#    precip_1hr       precipitation over the last hour [precipitation]
-#    precip_3hr       precipitation over the last 3 hours [precipitation]
-#    precip_6hr       precipitation over the last 6 hours [precipitation]
-#    precip_24hr      precipitation over the last 24 hours [precipitation]
-#
-#    _remarks         remarks [list of strings]
-#    _unparsed        unparsed remarks [list of strings]
-#
-#  The metar.Datatypes classes (temperature, pressure, precipitation,
-#  speed, direction) describe an observation and its units.  They provide
-#  value() and string() methods to that return numerical and string
-#  representations of the data in any of a number of supported units.
-#
-#  (You're going to have to study the source code for more details,
-#  like the available methods and supported unit conversions for the
-#  metar.Datatypes objects, etc..)
-
-#  In particular, look at the Metar.string()
-#  method, and the functions it calls.
-#
-#  Feb 4, 2005
-#  Tom Pollard
-#
-from metar import Metar
 import urllib
 import re
 import pytz
@@ -225,7 +146,7 @@ for id, name in sites.items():
 
         matchdew = re.match(dewPat, line)
         if matchdew:
-            print(matchdew.group(1))
+            #print(matchdew.group(1))
             appT_d=float(matchdew.group(1))
             dew = str(int(round(float(matchdew.group(1)))))
 
@@ -248,13 +169,16 @@ for id, name in sites.items():
     TD=array([float(appT_d)])
 
     apparentT = AppT(T, TD, W, T)
+    # print(apparentT[0])
+    # print(round(apparentT[0],0))
+    # print("\n----\n")
     appTVal=int(round(apparentT[0],0))
     if appTVal>90:
-        wcHI=str(appTVal) + " (hi)"
+        wcHI=str(appTVal) + " [HI]"
     elif appTVal<45 and windSpeed>4:
-        wcHI=str(appTVal) + " (wc)"
+        wcHI=str(appTVal) + " [WC]"
     else:
-        wcHI="(na)"
+        wcHI="na"
 
     row += 1
     obtable += '<td class="obtableft"><a href="' + href + '">' + name + '</a></td>'
